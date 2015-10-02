48472cda875d07975484f021d1be5dfe04bc5532
unknown <amish394@gmail.com>
2015-10-02 21:20:24 +0530

    Added Controller and Webconten Folder in Lican

---
+++ mainApp/Lican/src/app/Controller/Controller.java
@@ -0,0 +1,76 @@
+package app.controller;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+//@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Controller() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	System.out.println("Amish1");
+	String userName = request.getParameter("username");
+	String password = request.getParameter("password");
+	PrintWriter out=response.getWriter();
+	
+	if(userName.equals("Amish"))
+	{
+		if(password.equals("Amish"))
+		{
+			request.setAttribute("entry","Succed");
+			RequestDispatcher view = request.getRequestDispatcher("DashBoard.jsp");
+			view.forward(request,response);
+		}
+		else
+		{
+			out.println("Password is Incorrect");
+			RequestDispatcher view = request.getRequestDispatcher("Login.jsp");	
+
+		}
+		
+	}
+	else
+		
+	{
+		System.out.println("Username is Incorrect ");
+		RequestDispatcher view = request.getRequestDispatcher("Login.jsp");
+		
+
+	}
+	
+	
+	
+	}
+
+}
